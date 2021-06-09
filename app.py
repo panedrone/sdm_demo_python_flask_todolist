7e79acb8102be6f62e599d18fcd730f3f90e47a0
panedrone <sqldalmaker@gmail.com>
2021-06-09 09:36:32 +0300

    + requirements.txt

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, render_template
-from flask_restful import Api, Resource
+import flask
+import flask_restful
 
 from rest_utils import to_json_str
 from service import Service
 
-app = Flask(__name__)
-api = Api(app)
+app = flask.Flask(__name__)
+api = flask_restful.Api(app)
 
 
-class GroupListResource(Resource):
+class GroupListResource(flask_restful.Resource):
     @staticmethod
     def get():
         res = Service().get_groups()
@@ -17,11 +17,11 @@ class GroupListResource(Resource):
     @staticmethod
     def post():
         service = Service()
-        g_name = request.json["g_name"]
+        g_name = flask.request.json["g_name"]
         service.create_group(g_name)
 
 
-class GroupsResource(Resource):
+class GroupsResource(flask_restful.Resource):
     @staticmethod
     def get(g_id):
         res = Service().get_group(g_id)
@@ -29,7 +29,7 @@ class GroupsResource(Resource):
 
     @staticmethod
     def put(g_id):
-        g_name = request.json["g_name"]
+        g_name = flask.request.json["g_name"]
         Service().update_group(g_id, g_name)
 
     @staticmethod
@@ -37,21 +37,21 @@ class GroupsResource(Resource):
         Service().delete_group(g_id)
 
 
-class TaskListResource(Resource):
+class TaskListResource(flask_restful.Resource):
     @staticmethod
     def get():
-        g_id = request.args['g_id']
+        g_id = flask.request.args['g_id']
         res = Service().get_group_tasks(g_id)
         return to_json_str(res)
 
     @staticmethod
     def post():
-        g_id = request.args['g_id']
-        t_subject = request.json["t_subject"]
+        g_id = flask.request.args['g_id']
+        t_subject = flask.request.json["t_subject"]
         Service().create_task(g_id, t_subject)
 
 
-class TasksResource(Resource):
+class TasksResource(flask_restful.Resource):
     @staticmethod
     def get(t_id):
         task = Service().get_task(t_id)
@@ -61,7 +61,7 @@ class TasksResource(Resource):
     def put(t_id):
         service = Service()
         task = service.get_task(t_id)
-        inp = request.json
+        inp = flask.request.json
         task.t_date = inp["t_date"]
         task.t_subject = inp["t_subject"]
         task.t_priority = inp["t_priority"]
@@ -81,7 +81,7 @@ api.add_resource(TasksResource, '/tasks/<t_id>')
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return flask.render_template("index.html")
 
 
 if __name__ == "__main__":  # on running python app.py
